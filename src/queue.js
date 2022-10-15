c6a7190ad96971a7787780834fcb8501d847d0ce
Mariya Petrovskaya <pma190183@yandex.ru>
2022-10-15 19:50:59 +0300

    feat: queue

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,19 +14,50 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+}
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
+    let list = new ListNode(this.head.value);
+
+    let current = this.head;
+    let currentList = list;
+
+    while(current.next) {
+      currentList.next = current.next;
+      current = current.next;
+      currentList = currentList.next;
+    }
+
+    return list
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
+  enqueue(value) {
+    const node = new ListNode(value); // creates the node using class Node
+
+    if (this.head) { // if the first Node exitsts
+        this.tail.next = node; // inserts the created node after the tail of our Queue
+        this.tail = node; // now the created node is the last node
+    } else { // if there are no nodes in the Queue
+        this.head = node; // the created node is a head 
+        this.tail = node // also the created node is a tail in Queue because it is single.
+    }
+
+    this.length++; // increases the length of Queue by 1
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
+    const current = this.head; // saves the link to the head which we need to remove
+    this.head = this.head.next; // moves the head link to the second Node in the Queue
+    this.length--; // decreaments the length of our Queue
+    return current.value; // returns the removed Node's value
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 }
